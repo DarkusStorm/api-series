a87f5004933557d8d999acbaa3985634b6c672b4
DarkusStorm <tico.alvessilva2008@gmail.com>
2026-03-09 16:43:20 -0300

    16:43

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from api-series!")
+from fastapi import FastAPI
+from typing import Optional
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"mensagem": "Olá, mundo!"}
+
+@app.get("itens/{item_id}")
+async def read_item(item_id: int, q: Optional[str]):
+    return {"item_id": item_id, "q": q}
+
+# Crie uma rota que retorne a soma de dois números passados por um caminho (path de URL). Extra: melhore a tipagem do código usando tipos do módulo typing onde for necessário.
+
+@app.get("adicao/{a}/{b}")
+async def adicao(a: int, b: int):
+    return {"resultado": a + b}
